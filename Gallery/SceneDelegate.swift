ffc382947594cb51dfefc9d7f7f3a7ee20aef5a1
Jim Correia <correia@omnigroup.com>
2019-09-08 12:43:07 -0700

    Added code to demonstrate the bug, along with a README
    See details code and comment at SceneDelegate.sceneWillResignActive(_:).

--- Gallery/SceneDelegate.swift
+++ Gallery/SceneDelegate.swift
@@ -36,6 +36,58 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         return scene.userActivity
     }
     
+    /// The documentation for UIApplication.requestSceneSessionRefresh(_:) says:
+    ///
+    ///     Call this method when your scene is in the background and any
+    ///     part of your scene's visible appearance changes. For example,
+    ///     call this method after updating your scene's content to let the
+    ///     system know your scene's snapshot requires refreshing. You don't
+    ///     need to call this method when your scene is running in the
+    ///     foreground.
+    ///
+    /// The code in sceneWillResignActive(_:) simulates sync data coming in
+    /// to the application in way that requires the user interface to
+    /// update.
+    ///
+    /// If this event happens anytime after sceneWillResignActive(_:), but
+    /// before the scene has fully transitioned into the background, the
+    /// scene's window gets into a weird state where the snapshot is
+    /// blank/black. Bringing the scene to the foreground leaves it in this
+    /// unusable state, but switching back to the Home screen, then back
+    /// into the scene usually fixes it. Forcing the window to hidden then
+    /// visible also seems to "fix" it.
+    ///
+    /// I may have high expectations, but I expect to be able to call
+    /// requestSceneSessionRefresh(_:) in any scene state, and have UIKit:
+    ///
+    ///   - ignore requests made in the foreground state
+    ///   - efficiently handle requests made in other states (coalescing if required)
+    ///   - not "corrupt" the window and leave it in an unusable state
+    ///
+    /// The last is really the big issue, though, at this point.
+    ///
+    /// The workaround is to not issue the request refresh unless the scene
+    /// is fully in the .background state.
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            debugPrint("Scene activation state is \(scene.activationState.rawValue).")
+            
+            switch scene.activationState {
+            case .unattached, .foregroundActive:
+                debugPrint("Skipping request refresh based on scene state.")
+                break
+                
+            case .foregroundInactive, .background:
+                debugPrint("Requesting scene scession refresh.")
+                UIApplication.shared.requestSceneSessionRefresh(scene.session)
+                
+            @unknown default:
+                debugPrint("Skipping request refresh; unknown future state.")
+            }
+        }
+    }
+
     // Utilities
     
     func configure(window: UIWindow?, with activity: NSUserActivity) -> Bool {
--- README.md
+++ README.md
@@ -1,7 +1,30 @@
-# Supporting Multiple Windows on iPad
+Based on Apple [sample code](https://developer.apple.com/documentation/uikit/app_and_environment/scenes/supporting_multiple_windows_on_ipad).
 
-Use the UIScene lifecycle to support two side-by-side instances of your interface and learn how to create a new window with drag and drop.
+### Summary:
 
-## Overview
+Requesting a scene session refresh in certain states can cause scene windows to go blank/black.
 
-- Note: This sample code project is associated with WWDC 2019 session [212: Introducing Multiple Windows on iPad](https://developer.apple.com/videos/play/wwdc19/212/).
+### Steps to Reproduce:
+
+Build and run the attached sample code. See the code and detailed comment in SceneDelegate.sceneWillResignActive(_:).
+
+Once the application is running, transition slowly into the application picker, and then wait.
+
+Two seconds after you initiate the transition, the application request a scene refresh to simulate sync data coming in off the network that requires a UI state update.
+
+### Expected Results:
+
+The UI state updates. (In this sample app, there should be no visual change.)
+
+### Actual Results:
+
+The application window goes blank/black and becomes unusable.
+
+### Version/Build:
+
+iOS 13.1 b2
+
+### Configuration:
+
+This problem happens on all device/hardware classes.
+
